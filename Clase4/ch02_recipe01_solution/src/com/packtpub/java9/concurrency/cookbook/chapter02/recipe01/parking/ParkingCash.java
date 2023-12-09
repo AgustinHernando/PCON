4ab04dd064212d6b2452cc3792a52b06f68d1e12
Agustin Hernando <84819151+AgustinHernando@users.noreply.github.com>
2023-12-08 21:53:48 -0300

    some codes
    items added

---
+++ Clase4/ch02_recipe01_solution/src/com/packtpub/java9/concurrency/cookbook/chapter02/recipe01/parking/ParkingCash.java
@@ -0,0 +1,26 @@
+package com.packtpub.java9.concurrency.cookbook.chapter02.recipe01.parking;
+
+public class ParkingCash {
+
+	private static final int cost=2;
+	
+	private long cash;
+	
+	public ParkingCash() {
+		cash=0;
+	}
+	
+	public synchronized void vehiclePay() {
+		cash+=cost;
+	}
+	
+	public void close() {
+		System.out.printf("Closing accounting");
+		long totalAmount;
+		synchronized (this) {
+			totalAmount=cash;
+			cash=0;
+		}
+		System.out.printf("The total amount is : %d",totalAmount);
+	}
+}
